72e2013a7547a29c9d7b52569e2e2a99faec8a7c
tam11a <ibrahimsadiktamim@gmail.com>
2023-01-14 17:40:31 +0600

    initialized: theme

--- src/layouts/Website/Header/index.tsx
+++ src/layouts/Website/Header/index.tsx
@@ -1,7 +1,62 @@
+import {
+	AppBar,
+	Avatar,
+	Box,
+	Container,
+	ListItemText,
+	Toolbar,
+} from "@mui/material";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 const WebsiteHeader: React.FC = () => {
-	return <div>Header</div>;
+	return (
+		<>
+			<Routes>
+				<Route
+					path=""
+					element={<></>}
+				/>
+				<Route
+					path="*"
+					element={
+						<>
+							<AppBar>
+								<Toolbar sx={{ bgcolor: "background.paper" }}>
+									<Container
+										maxWidth={"lg"}
+										className="flex flex-row items-center"
+									>
+										<Avatar
+											src="/favicon.svg"
+											variant="square"
+										/>
+										<ListItemText
+											primary={"CodeBin"}
+											secondary={"Share Code as You Want"}
+											className="ml-4"
+											primaryTypographyProps={{
+												sx: {
+													color: "secondary.light",
+													fontWeight: "bold",
+													textTransform: "uppercase",
+													letterSpacing: 2,
+												},
+											}}
+											secondaryTypographyProps={{
+												sx: { color: "#ffffff99", fontWeight: "light" },
+											}}
+										/>
+									</Container>
+								</Toolbar>
+							</AppBar>
+							<Box height={"68px"} />
+						</>
+					}
+				/>
+			</Routes>
+		</>
+	);
 };
 
 export default WebsiteHeader;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,42 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({});
+const theme = createTheme({
+	typography: {
+		fontFamily: "Sofia Sans, sans-serif",
+	},
+	palette: {
+		primary: {
+			main: "#9DD6FC",
+			light: "#b1defd",
+			dark: "#7eabca",
+			contrastText: "#2E3440",
+		},
+		secondary: {
+			main: "#D17FF7",
+			light: "#dfa5f9",
+			dark: "#9259ad",
+			contrastText: "#2E3440",
+		},
+		success: {
+			light: "#e2faec",
+			main: "#35df90",
+			dark: "#007626",
+		},
+		warning: {
+			light: "#fef3df",
+			main: "#fab648",
+			dark: "#e15000",
+		},
+		error: {
+			light: "#f9e6e6",
+			main: "#f47a61",
+			dark: "#b1160d",
+		},
+		background: {
+			default: "#2E3440",
+			paper: "#434C5E",
+		},
+	},
+});
 
-export default theme
+export default theme;
